452cadc7fca2bdf432339382b3bc4fe69bfcade1
MananSharma12 <manan.sharma1205@gmail.com>
2024-06-18 22:53:19 +0530

    feat: add addCustomer button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,12 @@
+import { Button } from "~/components/ui/button";
+
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div class="flex items-center justify-between">
+      <h1 class="text-2xl font-semibold">Dashboard</h1>
+      <Button size="sm" onClick={() => {}}>
+        Add Customer
+      </Button>
+    </div>
+  );
 };
